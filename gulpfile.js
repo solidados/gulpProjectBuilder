b39c4707766202a7ac627d2190ab275558c4425a
solidados <geekbrains.pk@internet.ru>
2022-04-12 12:13:43 +0300

    added const for paths and folders structure

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,15 @@ const less = require('gulp-less');
 const del = require('del');
 const cleanCSS = require('gulp-clean-css');
 
+// building folders structure
 const paths = {
     styles: {
-        src: 'src/styles/**/*.less'
+        src: 'src/styles/**/*.less',
+        dest: 'dist/css/'
+    },
+    scripts: {
+        src: 'src/scripts/**/*.js',
+        dest: 'dist/js/'
     }
 }
 
