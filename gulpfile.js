0781a6ccae043aed49fae8d4ae46aa5c6408737a
solidados <geekbrains.pk@internet.ru>
2022-04-12 12:17:30 +0300

    interim commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
-const gulp = require('gulp');
-const less = require('gulp-less');
-const del = require('del');
-const cleanCSS = require('gulp-clean-css');
+const gulp = require('gulp')
+const less = require('gulp-less')
+const del = require('del')
+const cleanCSS = require('gulp-clean-css')
 
 // building folders structure
 const paths = {
