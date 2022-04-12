3f64be8651c3c3e74017cd699728a48ea9b12701
solidados <geekbrains.pk@internet.ru>
2022-04-12 12:06:48 +0300

    const branch started for gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,15 @@ const less = require('gulp-less');
 const del = require('del');
 const cleanCSS = require('gulp-clean-css');
 
+const paths = {
+    styles: {
+        src: 'src/styles/**/*.less'
+    }
+}
+
+// function to clean /dist folder
 function clean() {
     return del(['dist'])
-
 }
+
+exports.clean = clean
